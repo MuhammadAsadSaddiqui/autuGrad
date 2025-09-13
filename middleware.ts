99a76349c0c67ea27dd3f76c8367bdec3e45435a
asad <asaddd1576@gmail.com>
2025-09-13 20:32:42 +0500

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,37 @@
+// middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 
 const privateRoutes = [
   "/dashboard",
-  "/dashboard",
   "/profile",
   "/settings",
   "/unverified",
+  "/quiz",
 ];
 
 const authRoutes = ["/login", "/register", "/forgotpass", "/signup"];
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  if (!privateRoutes.includes(path) && !authRoutes.includes(path)) {
+  
+  if (!privateRoutes.some(route => path.startsWith(route)) && !authRoutes.includes(path) && path !== "/") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+  
   const token =
     request.cookies.get("next-auth.session-token")?.value ||
     request.cookies.get("__Secure-next-auth.session-token")?.value;
+    
   if (!token && privateRoutes.some((route) => path.startsWith(route))) {
     const loginUrl = new URL("/login", request.url);
     loginUrl.searchParams.set("callbackUrl", path);
     return NextResponse.redirect(loginUrl);
   }
+  
   if (token && authRoutes.some((route) => path === route)) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+  
   return NextResponse.next();
 }
 
